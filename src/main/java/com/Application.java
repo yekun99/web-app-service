d3208940cb1b1e53790e06cd97a2380e0018a48f
De Chun Yuan <yuandechunsh@163.com>
2020-06-30 18:16:26 +0800

    add redis config

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,12 +2,17 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+
+@EnableCaching
+@EnableRedisHttpSession
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
