8df0a0a82a64c6b64940cc053d8e33eed6a4fd6d
De Chun Yuan <yuandechunsh@163.com>
2020-11-19 12:40:42 +0800

    update config

--- src/main/java/com/app/commtaskmanage/repository/CommTaskHisRepository.java
+++ src/main/java/com/app/commtaskmanage/repository/CommTaskHisRepository.java
@@ -2,6 +2,7 @@ package com.app.commtaskmanage.repository;
 
 import com.core.data.model.DataModel;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * @author dechun.yuan
  * @version 1.0
  */
+@Repository
 @Mapper
 public interface CommTaskHisRepository {
 
